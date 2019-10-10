f454859ed7d71fde9b298ac37cbf05aee46f8f3e
duongpv <duongpv@vng.com.vn>
2019-10-10 11:17:58 +0700

    call api youtube

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const app = express()
+const request = require('request')
+
+const api = "https://www.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyDzdwSsJFG8tD8wmAQTKpZDBndfUm8DDxs&id="
 
 app.set('view engine', 'pug')
 app.set('views', './views')
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/songs/:id', (req, res) => {
+  request.get({
+    url: api + req.params.id,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }, (error, response, body) => {
+    if (response.statusCode == 200) {
+      const jsonBody = JSON.parse(body).items[0]
+      res.render('index', {
+        songTitle: jsonBody.snippet.title + jsonBody.snippet.channelTitle,
+        songDescription: jsonBody.snippet.description,
+        songThumbnail: jsonBody.snippet.thumbnails.high.url
+      })
+    } else {
+      console.log(error)
+      res.send("Muzic - Music every where")
+    }
+  })
 })
- 
+
 app.listen(process.env.PORT || 3000)
